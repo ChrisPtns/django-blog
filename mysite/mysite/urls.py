8e31b33a7d47113f53e91cc525ae22fb60390c98
ChrisPtns <chris.pitsinis@gmail.com>
2026-06-24 14:19:53 +0300

    Complete Django Blog Part 2: Views, URLs, Templates and Admin

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,12 +1,7 @@
-"""
-URL configuration for mysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
